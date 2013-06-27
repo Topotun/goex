a5f9ca533f07d58189ef7c25d07a42a65e541ba7
Topotun <lmolokov@gmail.com>
2013-06-27 15:45:28 +0200

    Hanging client can be deliberately introduced

--- client.go
+++ client.go
@@ -7,9 +7,10 @@ import (
 	"log"
 	"math/rand"
 	"net"
+	"time"
 )
 
-func client(addr, name string) {
+func client(addr, name string, hangie bool) {
 	/*Starts a client, initiates a connection*/
 	conn, err := net.Dial(network, addr+port)
 	var succ int
@@ -34,26 +35,33 @@ func client(addr, name string) {
 		}
 	}
 	/*Now we are sending some numbuffer = make([]byte, buffer_size)ber of requests*/
-	for j := 0; j < 2*max_requests; j++ {
-		buffer = make([]byte, buffer_size)
-		number := uint64(rand.Uint32() % 10000)
-		fmt.Println("My name is", name, "I am sending number", number, "on attempt", j)
-		binary.PutUvarint(buffer, number)
-		_, err = write_deadline(conn, waiting_time, buffer)
-		if nil != err {
-			log.Println("Failed to write to server")
-			continue //we hope to recover in the future
+	if !hangie { //good client
+		for j := 0; j < 2*max_requests; j++ {
+			buffer = make([]byte, buffer_size)
+			number := uint64(rand.Uint32() % 10000)
+			fmt.Println("My name is", name, "I am sending number", number, "on attempt", j)
+			binary.PutUvarint(buffer, number)
+			_, err = write_deadline(conn, waiting_time, buffer)
+			if nil != err {
+				log.Println("Failed to write to server")
+				continue //we hope to recover in the future
+			}
+			_, err = read_deadline(conn, waiting_time, buffer)
+			if nil != err {
+				log.Println("Failed to read from server")
+				continue //we hope to recover in the future
+			}
+			number, succ = binary.Uvarint(buffer)
+			if succ < 1 {
+				fmt.Println("My name is", name, "I failed to get a sensible answer from server on attempt", j, "!")
+			} else {
+				fmt.Println("My name is", name, "I have got the number", number, "on attempt", j)
+			}
 		}
-		_, err = read_deadline(conn, waiting_time, buffer)
-		if nil != err {
-			log.Println("Failed to read from server")
-			continue //we hope to recover in the future
-		}
-		number, succ = binary.Uvarint(buffer)
-		if succ < 1 {
-			fmt.Println("My name is", name, "I failed to get a sensible answer from server on attempt", j, "!")
-		} else {
-			fmt.Println("My name is", name, "I have got the number", number, "on attempt", j)
+	} else { //terrible client, deserving to be dropped
+		for {
+			fmt.Println("My name is", name, "I am trying to hang the server")
+			time.Sleep(time.Second)
 		}
 	}
 	defer conn.Close()
